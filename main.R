b976ebbab519b5b3b59a88b1def7e7bf69950c88
Louise Searle <Louise@LS01.local>
2015-01-07 16:57:31 +0100

    Complete main programme with annotation.

--- main.R
+++ main.R
@@ -4,3 +4,17 @@
 ## Source functions
 
 source('R\isLeapYear.R')
+
+## Commands
+
+# Example returns 'TRUE'
+isLeapYear(2004)
+
+# Example returns 'FALSE'
+isLeapYear(2001)
+
+# Example returns '1087 is out if range'
+isLeapYear(1001)
+
+# Example returns 'error: arguement of class numeric expected'
+isLeapYear('louise')
